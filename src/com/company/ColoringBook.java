356483160b9cd0b432829655d14a6e0b1db9f690
гк <lisitsa13ht@gmail.com>
2022-11-03 21:27:09 +0300

    Initial commit

---
+++ src/com/company/ColoringBook.java
@@ -0,0 +1,66 @@
+package com.company;
+
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ColoringBook {
+
+    public static void main(String[] args) throws IOException {
+        JFrame jFrame = new JFrame("Flood fill");
+        jFrame.setSize(500, 300);
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setContentPane(new DrawPane());
+        jFrame.repaint();
+        jFrame.setVisible(true);
+
+    }
+}
+
+class DrawPane extends JPanel {
+     BufferedImage bufferedImage = null;
+     int color;
+
+
+    DrawPane() throws IOException {
+        File file = new File("src/com/company/image2.jpg");
+        bufferedImage = ImageIO.read(file);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                color = bufferedImage.getRGB(x, y);
+                //bufferedImage.setRGB(x, y, new Color(255, 0, 0).getRGB());
+               floodFill(x,y);
+                System.out.printf("x= %d,y=%d, color=%d%n", x, y, color);
+                repaint();
+            }
+        });
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(bufferedImage, 0, 0, null);
+    }
+
+    void floodFill(int x, int y) {
+        int colorCurr = bufferedImage.getRGB(x, y);
+        if (color!=colorCurr)return;
+        bufferedImage.setRGB(x, y, new Color(255, 0, 0).getRGB());
+        floodFill(x-1,y);
+        floodFill(x+1,y);
+        floodFill(x,y-1);
+        floodFill(x,y+1);
+    }
+}
+
